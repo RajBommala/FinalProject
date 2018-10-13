9982b0b613bcbcaa9405825e1fadd25351202ec1
Bommala <basusmy@gmail.com>
2018-10-13 13:33:04 -0500

    components updated

--- app/src/components/Signin/Signin.js
+++ app/src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://mysterious-ravine-53628.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
