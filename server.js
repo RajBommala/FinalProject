4e309547d91d55435bd065b37dfb1caabcc75340
Bommala <basusmy@gmail.com>
2018-10-17 21:55:13 -0500

    server.js updated

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'basavaraja',
-    password : '',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
+
+db.select('*')
+  .from('users')
+  .then(data => {});
 
 const app = express();
 
@@ -31,6 +33,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3001, ()=> {
-  console.log('app is running on port 3001');
-})
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+});
